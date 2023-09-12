7ec77f235d76dfb1df3872598f9f81dc352cdc59
Edson Quispe <edsonqv2411@gmail.com>
2023-09-12 12:04:33 -0400

    hice que la lista de contactos tenga mas elementos por contacto, convirtiendolos de array a objetos

--- contactos.js
+++ contactos.js
@@ -1,19 +1,42 @@
 // 1
 let contactos = [
-  "Jasmin Pimentel",
-  "Andres Cadima",
-  "Laura Grageda",
-  "Ismael Callejas",
+  {
+    id: 5960927,
+    nombre: "Edson",
+    apellidos: "Quispe Valencia",
+    telefono: 60357555,
+    ubicaciones: [{ ciudad: "Cochabamba", direccion: "Av Villarroel" }],
+  },
+  {
+    id: 1234567,
+    nombre: "Jasmin",
+    apellidos: "Pimentel Chavez",
+    telefono: 77909871,
+    ubicaciones: [{ ciudad: "Cochabamba", direccion: "Av Pando" }],
+  },
+  {
+    id: 7654321,
+    nombre: "Andres",
+    apellidos: "Cadima Zeballos",
+    telefono: 79724780,
+    ubicaciones: [{ ciudad: "Cochabamba", direccion: "Av Perú" }],
+  },
 ];
 
 // 2
-function holaContactos(nombreApellido) {
-  contactos.push(nombreApellido);
+function holaContacto(id, nombre, apellidos, telefono, ubicaciones) {
+  contactos.push({
+    id: id,
+    nombre: nombre,
+    apellidos: apellidos,
+    telefono: telefono,
+    ubicaciones: ubicaciones,
+  });
 }
 
 // 3
-function chauContactos(nombreApellido) {
-  let index = contactos.indexOf(nombreApellido);
+function chauContacto(nombre) {
+  let index = contactos.findIndex((contacto) => contacto.nombre === nombre);
   if (index !== -1) {
     contactos.splice(index, 1);
   }
@@ -21,7 +44,16 @@ function chauContactos(nombreApellido) {
 
 // 4
 function contactosLog() {
-  for (let i = 0; i < contactos.length; i++) {
-    console.log(contactos[i]);
-  }
+  contactos.forEach((contacto) => {
+    console.log(`ID: ${contacto.id}`);
+    console.log(`Nombre: ${contacto.nombre}`);
+    console.log(`Apellidos: ${contacto.apellidos}`);
+    console.log(`Teléfono: ${contacto.telefono}`);
+    console.log(`Ubicaciones:`);
+    contacto.ubicaciones.forEach((ubicacion) => {
+      console.log(`Ciudad: ${ubicacion.ciudad}`);
+      console.log(`Dirección: ${ubicacion.direccion}`);
+    });
+    console.log("---");
+  });
 }
